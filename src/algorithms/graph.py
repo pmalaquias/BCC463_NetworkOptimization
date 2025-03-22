ce19b3c4086c77f65275810bb514ae69c538a244
Pedro Igor <40576393+pmalaquias@users.noreply.github.com>
2025-03-22 12:22:14 -0300

    enhance Graph class documentation with detailed attribute and method descriptions

--- src/algorithms/graph.py
+++ src/algorithms/graph.py
@@ -1,12 +1,22 @@
 # Grafo estruturado como lista de adjacências
 
 class Graph:
-    """Classe que representa um grafo"""
-    #n = número de vértices
-    #directed = grafo direcionado ou não
-    #adj = lista de adjacências
-    #capacity = capacidade das arestas
-    #explored_edges = arestas exploradas
+    """""
+    Graph class represents a graph data structure with support for directed and undirected graphs.
+    Attributes:
+        n (int): Number of vertices in the graph.
+        directed (bool): Indicates whether the graph is directed or not.
+        adj (list of list): Adjacency list representation of the graph.
+        capacity (dict): Dictionary storing the capacity (weight) of edges as key-value pairs.
+        explored_edges (set): Set to keep track of explored edges.
+    Methods:
+        __init__(n, directed):
+            Initializes the graph with the given number of vertices and directionality.
+        add_edge(u, v, w):
+            Adds an edge from vertex u to vertex v with weight w. If the graph is undirected,
+            it also adds the reverse edge.
+        show_graph():
+    """""
 
     def __init__(self, n, directed):
         self.n = n # número de vértices
@@ -25,6 +35,18 @@ class Graph:
             self.capacity[(v, u)] = w
 
     def show_graph(self):
+        """
+        Displays the adjacency list representation of the graph.
+
+        For each vertex in the graph, this method prints the vertex followed by
+        the list of its adjacent vertices.
+
+        Example output for a graph with 3 vertices and edges (0 -> 1, 0 -> 2, 1 -> 2):
+            0 -> 1 2
+            1 -> 2
+            2 ->
+
+        """
         for u in range(self.n):
             print(u, end=' -> ')
             for v in self.adj[u]:
