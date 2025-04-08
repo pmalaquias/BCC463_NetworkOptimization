74524fce9391622a64f52e478a68b0480f957ac6
Pedro Igor <40576393+pmalaquias@users.noreply.github.com>
2025-04-08 09:24:11 -0300

    refactor: restructure main function to utilize menu-driven graph algorithm selection

--- src/main.py
+++ src/main.py
@@ -6,57 +6,67 @@ from algorithms.BFS import BFS
 from algorithms.DFS import DFS
 from algorithms.Dijkstra import Dijkstra
 from algorithms.graph import Graph
+from algorithms.Johnson import johnson
 from algorithms.mostrar_resultados import mostrar_resultado
-from algorithms.readFile import read_file
+from utils.load_graph import load_graph
+from utils.menu import menu
+from utils.readFile import read_file
 
 
 def main():
-    # Caminho do arquivo no diretório ../input/
-    diretorio_base = os.path.dirname(os.path.abspath(__file__))  # Diretório do script
-    caminho_arquivo = os.path.join(diretorio_base, "../input/input3.txt")
+
+    menu()
+    menu_option = int(input("Digite sua opção: "))
+
+    while menu_option != 0:
+
+        if menu_option < 0 or menu_option > 6:
+            print("Opção inválida!")
+            menu()
+            menu_option = int(input("Digite sua opção: "))
+            continue
+
+        # Carregar o grafo com base na opção do menu
+        g, i = load_graph(menu_option, read_file, Graph)
 
 
+        if menu_option == 1:
+            print('Grafo:')
+            g.show_graph()
+        elif menu_option == 2:
+            print("DFS:")
+            visited = [False] * (g.n + 1)
+            result = []
+            DFS(g, i, visited, result)
+            print(" ".join(map(str, result)))
+        elif menu_option == 3:
+            print("BFS:")
+            BFS(g, i)
+        elif menu_option == 4:
+            print("Dijkstra:")
+            dt, rot = Dijkstra(g, i)
+            mostrar_resultado(dt, rot, i)
+        elif menu_option == 5:
+            print("Bellman-Ford:")
+            dt, rot, has_negative_cycle = BellmanFord(g, i)
+            if not has_negative_cycle:
+                mostrar_resultado(dt, rot, i)
+        elif menu_option == 6:
+            print("Johnson:")
+            matriz = johnson(g)
+            if matriz:
+                for linha in matriz:
+                    print(' '.join(str(int(x)) if x != float('inf') else 'INF' for x in linha))
+        elif menu_option == 0:
+            print("Saindo...")
+            break
 
-    entrada = read_file(caminho_arquivo)
-
-    # Processamento dos dados de entrada
-    n, m, b, i = map(int, entrada[0].split()) #lendo os valores de n, m, b e i
-    g = Graph(n, directed=bool(b)) #criando o grafo
-
-    for row in entrada[1:]:
-        u, v, w = map(int, row.split())
-        g.add_edge(u, v, w)
-
-    #mostrando o grafo
-    print('Grafo:')
-    g.show_graph()
-
-    # #realizando a busca em profundidade
-    # print("DFS:")
-    # visited = [False] * (g.n + 1)
-    # result = []
-    # DFS(g, i, visited, result)
-    # print(" ".join(map(str, result))) #imprimindo o resultado
-
-    # #realizando a busca em largura
-    # print("BFS:")
-    # BFS(g, i)
-
-    # #realizando o algoritmo de Dijkstra
-    # print("Dijkstra:")
-    # #importando a função Dijkstra
-    # dt, rot =Dijkstra(g, i)
-    # mostrar_resultado(dt, rot, i)
-
-    # realizando o algoritmo de Bellman-Ford
-    print("Bellman-Ford:")
-    # importando a função BellmanFord
-    dt, rot, has_negative_cycle = BellmanFord(g, i)
-    
-    if not has_negative_cycle:
-        mostrar_resultado(dt, rot, i)
-
+        print("\n")
+        menu()
+        menu_option = int(input("Digite sua opção: "))
+       
 
 if __name__ == '__main__':
+    os.system('cls' if os.name == 'nt' else 'clear')
     #print('This is the main module')
     main() 
