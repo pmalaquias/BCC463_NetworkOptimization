26fbb35f30c43a67ab8bbf7be9c99104d027b0d2
Pedro Igor <40576393+pmalaquias@users.noreply.github.com>
2025-04-10 17:41:44 -0300

    refactor: rename function to BuildPath and enhance docstring for clarity

--- src/algorithms/reconstruir_caminho.py
+++ src/algorithms/reconstruir_caminho.py
@@ -1,4 +1,16 @@
-def reconstruir_caminho( rot, destino):
+def BuildPath( rot, destino):
+    """
+        Reconstrói o caminho de um nó de origem até um nó de destino usando um dicionário de roteamento.
+
+        Argumentos:
+            rot (dict): Um dicionário onde as chaves são nós e os valores são o nó precedente 
+                        no caminho até a origem. O valor para o nó de origem deve ser None.
+            destino: O nó de destino para o qual o caminho será reconstruído.
+
+        Retorna:
+            list: Uma lista de nós representando o caminho da origem até o destino, 
+                  em ordem da origem para o destino.
+    """
     caminho =[]
     atual = destino
     while atual is not None:
